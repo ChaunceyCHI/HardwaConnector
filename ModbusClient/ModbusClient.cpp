90a2b9ca6ed7b8ff344d1a50db33d6f6bfd03bec
ChaunceyCHI <Alex_loveyou@yeah.net>
2025-01-23 00:36:29 +0800

    创建工程

---
+++ ModbusClient/ModbusClient.cpp
@@ -0,0 +1,14 @@
+/*
+* File:  ModbusClient.cpp
+* Author: [ChaunceyCHI]
+* Created Date: [2025-01-03]
+* 
+* Description: 
+* This file implements the functionality of a Modbus client, including 
+* communication with a Modbus server, data reading, and writing operations.
+* 
+* Modification History:
+* Date        Author        Description
+* ----------  ----------  -----------------------------------
+* [Mod Date]  [Mod Author]  [Modification Description]
+*/
---
+++ ModbusClient/ModbusClient.h
@@ -0,0 +1,22 @@
+/*
+* File:  ModbusClient.h
+* Author: [ChaunceyCHI]
+* Created Date: [2025-01-03]
+*
+* Description:
+* This file declares the functionality of a Modbus client, including
+* communication with a Modbus server, data reading, and writing operations.
+*
+* Modification History:
+* Date        Author        Description
+* ----------  ----------  -----------------------------------
+* [Mod Date]  [Mod Author]  [Modification Description]
+*/
+#pragma once
+
+#ifndef MODBUS_CLIENT_H
+#define MODBUS_CLIENT_H
+
+
+
+#endif // MODBUS_CLIENT_H
